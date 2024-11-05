2c269c03893b47ed2fbd980d81478eb5f535be39
Lukasz MAzurek <mazione@pop-os.home>
2024-11-05 01:24:41 +0000

    added error handling

--- utils/db.js
+++ utils/db.js
@@ -8,47 +8,97 @@ class Db {
   }
 
   async _load() {
-    this._data = JSON.parse(await readFile(this.dbFileName, 'utf-8'));
+    try {
+      this._data = JSON.parse(await readFile(this.dbFileName, 'utf-8'));
+    } catch (err) {
+      throw new Error('Failed to load the database');
+    }
   }
 
   _save() {
-    writeFile(this.dbFileName, JSON.stringify(this._data), 'utf-8');
+    try {
+      writeFile(this.dbFileName, JSON.stringify(this._data), 'utf-8');
+    } catch (err) {
+      throw new Error('Failed to save the database');
+    }
   }
 
   create(obj) {
-    const id = uuid();
-    this._data.push({
-      id: id,
-      ...obj,
-    });
-    this._save();
-    
-    return id;
+    try {
+      if (!obj || !obj.name || !obj.mail) {
+        const error = new Error('Name and email are required');
+        error.statusCode = 400;
+        throw error;
+      }
+      const id = uuid();
+      this._data.push({
+        id: id,
+        ...obj,
+      });
+      this._save();
+      return id;
+    } catch (err) {
+      throw err;
+    }
   }
 
   getAll() {
-    return this._data;
+    try {
+      return this._data;
+    } catch (err) {
+      throw new Error('Failed to fetch all data');
+    }
   }
 
   getOne(id) {
-    return this._data.find((oneObj) => oneObj.id === id);
+    try {
+      const record = this._data.find(oneObj => oneObj.id === id);
+      if (!record) {
+        const error = new Error(`User with id: ${id} not found`);
+        error.statusCode = 404;
+        throw error;
+      }
+      return record;
+    } catch (err) {
+      throw err;
+    }
   }
 
   update(id, newObj) {
-    this._data = this._data.map((oneObj) =>
-      oneObj.id === id
-        ? {
-            ...oneObj,
-            ...newObj,
-          }
-        : oneObj
-    );
-    this._save();
+    try {
+      const index = this._data.find(oneObj => oneObj.id === id);
+      if (index === -1) {
+        const error = new Error(`User with id: ${id} not found`);
+        error.statusCode = 404;
+        throw error;
+      }
+      this._data = this._data.map(oneObj =>
+        oneObj.id === id
+          ? {
+              ...oneObj,
+              ...newObj,
+            }
+          : oneObj
+      );
+      this._save();
+    } catch (err) {
+      throw err;
+    }
   }
 
   delete(id) {
-    this._data = this._data.filter((obj) => obj.id !== id);
-    this._save();
+    try {
+      const index = this._data.find(oneObj => oneObj.id === id);
+      if (index === -1) {
+        const error = new Error(`User with id: ${id} not found`);
+        error.statusCode = 404;
+        throw error;
+      }
+      this._data = this._data.filter(obj => obj.id !== id);
+      this._save();
+    } catch (err) {
+      throw err
+    }
   }
 }
 const db = new Db('client.json');
